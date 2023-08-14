067fcd700f2f76b1c7c41b18aa850072b3a594bc
Paulo Santana <paulosantana.sa@gmail.com>
2023-08-14 00:48:24 -0300

    feat: Add `Home page` component.

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,59 @@
+import { Play } from "phosphor-react";
+import {
+  CountdownButton,
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  TaskInput,
+} from "./styles";
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="1" />
+            <option value="2" />
+            <option value="3" />
+            <option value="Banana" />
+          </datalist>
+
+          <label htmlFor="minutesAmout">durante</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmout"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <CountdownButton disabled type="submit">
+          <Play size={24} />
+          Começar
+        </CountdownButton>
+      </form>
+    </HomeContainer>
+  );
+}
